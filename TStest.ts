3bbce7acde5af1b43fe9f6b301342be50f3a8594
aguk98 <aguk98@outlook.com>
2022-06-25 20:44:13 -0300

    commit ajsf

---
+++ TStest.ts
@@ -0,0 +1,24 @@
+class <nombre>{
+    /*atributos*/
+    /*metodos*/
+}
+
+class Personal {
+    //Atributoss de la clase Persona
+    nombre:string;s
+    apellido:string;
+    anyoNac:number;
+}
+
+//                               :void si no se quiere devolver nada
+EdadAproximada(anyoActual:number):number {
+    //Metodoss
+    return anyoActual - this.anyoNac;
+}
+
+constructor(nombre:string, apepllido:string) {
+    //Constructoresss
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
